15f1218e282b10d89ba23ee3b222ad66cb152323
yarden <yarden.tulchinsky@gmail.com>
2024-06-29 12:28:45 +0300

    get all the posts in start of app and create a list of post objects with Post class. after clicking on link get the post id and use route to show the post page

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from flask import Flask, render_template
 import datetime as dt
 import requests
+from post import Post
 
 app = Flask(__name__)
+
+response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+response.raise_for_status()
+all_posts = response.json()
+
+posts_list: list = []
+
+for post in all_posts:
+    new_post = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    posts_list.append(new_post)
 
 
 @app.route('/')
 def get_home():
-    response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
-    response.raise_for_status()
-    posts = response.json()
     year = dt.datetime.now().year
-    return render_template('index.html', year=year, posts=posts)
+    return render_template('index.html', year=year, posts=posts_list)
+
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    for post in posts_list:
+        if post.id == post_id:
+            return render_template('post.html', post=post)
 
 
 if __name__ == '__main__':
